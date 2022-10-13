874619a2cf66cf9bdcffd07aea6598b1b88a818f
Bill Vi <billvi2002@hotmail.nl>
2022-10-13 19:34:39 +0200

    Removed duplicated in dataframes

--- Data processing.R
+++ Data processing.R
@@ -15,7 +15,10 @@ vector_dealnumber <- as.vector(t(ford_dealnumber))
 # List of companies which have/had a Joint Venture with Ford.
 ford_jointventures <- sdc %>% filter(deal_number %in% vector_dealnumber,
     SIC_primary != "999A", #Remove countries from Joint Ventures
-    participants != "Ford Motor Co") #Removes Ford from the list
+    participants != "Ford Motor Co") %>% #Removes Ford from the list
+    select(participants, business_description, participant_nation)
+ford_jointventures <- ford_jointventures %>%
+    distinct(participants, .keep_all = TRUE) # Removes duplicates
 
 # Filter the set with only BMW and their deal_number
 bmw_dealnumber <- sdc %>% filter(type == "Joint Venture",
@@ -26,7 +29,10 @@ vector_dealnumber_bmw <- as.vector(t(bmw_dealnumber))
 # List of companies which have/had a Joint Venture with BMW.
 bmw_jointventures <- sdc %>% filter(deal_number %in% vector_dealnumber_bmw,
     SIC_primary != "999A", #Remove countries from Joint Ventures
-    participants != "Bayerische Motoren Werke AG") #Removes BMW from the list
+    participants != "Bayerische Motoren Werke AG") %>% #Removes BMW
+    select(participants, business_description, participant_nation)
+bmw_jointventures <- bmw_jointventures %>%
+    distinct(participants, .keep_all = TRUE) # Removes duplicates
 
 # Filter the set with only Toyota and their deal_number
 toyota_dealnumber <- sdc %>% filter(type == "Joint Venture",
@@ -38,4 +44,9 @@ vector_dealnumber_toyota <- as.vector(t(toyota_dealnumber))
 toyota_jointventures <- sdc %>% filter(
     deal_number %in% vector_dealnumber_toyota,
     SIC_primary != "999A", #Remove countries from Joint Ventures
-    participants != "Toyota Motor Corp") #Removes BMW from the list
+    participants != "Toyota Motor Corp") %>% #Removes Toyota from the list
+    select(participants, business_description, participant_nation)
+toyota_jointventures <- toyota_jointventures %>%
+    distinct(participants, .keep_all = TRUE) # Remove duplicates
+
+# Create adjacency matrices of the dataframes
